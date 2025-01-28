9cf87e29eb18c4bed910e84141b9f2b0ad61b2c5
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-01-28 13:24:24 +0300

    add about section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 	<HeaderComp />
 	<HeroSection />
 	<TitlesBar />
+	<AboutSection />
 </template>
 
 <script setup lang="ts">
 import HeaderComp from '@/components/HeaderComp.vue';
 import HeroSection from '@/components/HeroSection.vue';
 import TitlesBar from '@/components/TitlesBar.vue';
+import AboutSection from '@/components/sections/AboutSection.vue';
 </script>
